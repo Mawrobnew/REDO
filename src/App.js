6ec8451952d5000d54a27b9e1425c715bd7200e3
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-04-29 11:39:09 -0500

    If the user is not authenticated the app redirect to the login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import Menu from './component/Menu';
 import T_User from "./component/T_User";
@@ -6,16 +6,25 @@ import T_GlobalBeneficiary from "./component/T_GlobalBeneficiary";
 import T_BeneficiaryDocuments from "./component/T_BeneficiaryDocuments";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./component/Login"
+import { useNavigate } from 'react-router-dom';
 
+let MainPage = ()=>{
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const token = sessionStorage.getItem("token")
+        if(token === null) {
+            navigate('/login')
+        }
+    },[])
+
+    return <div>
+        <Menu/>
+        <T_User/>
+        <T_GlobalBeneficiary/>
+        <T_BeneficiaryDocuments/>
+    </div>
+}
 function App() {
-    let MainPage = ()=>(
-        <div>
-            <Menu/>
-            <T_User/>
-            <T_GlobalBeneficiary/>
-            <T_BeneficiaryDocuments/>
-        </div>
-    )
   return (
       <Router>
       <Routes>
