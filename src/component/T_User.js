d0eb38579eff3a3f8a1c234e6ed52de4b10809a4
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-05-19 23:07:34 -0500

    Modify user added functionality

--- src/component/T_User.js
+++ src/component/T_User.js
@@ -23,8 +23,8 @@ function T_User(){
     }
 
     const btnModUSer = (cell, row, rowIndex, formatExtraData) => {
-        const {Nombre, Correo, Numero, Id} = row
-        const info = {name: Nombre, mail: Correo, phone: Numero, id: Id}
+        const {Nombre, Correo, Numero, Id, IdRol, IdSucursal} = row
+        const info = {name: Nombre, mail: Correo, phone: Numero, id: Id, rol: IdRol, branch: IdSucursal}
         return (
             <div>
                 <M_ModUser open={isOpen} onClose={setIsOpen} userinfo={info}/>
@@ -39,17 +39,26 @@ function T_User(){
             </div>
         );
     };
+    const btnModUserPass = (cell, row, rowIndex, formatExtraData) => {
+        return (
+            <div>
+                <M_ModUserPass/>
+            </div>
+        );
+    };
 
     const columns = [
-        {dataField:'Id', text:'Id', sort:true, key:1, hidden:true},
-        {dataField:'Nombre', text:'Nombre', sort:true, key:2},
-        {dataField:'Correo', text:'Correo', sort:true, key:3},
-        {dataField:'Numero', text:'Telefono', sort:true, key:4},
-        {dataField:'Rol', text:'Rol', sort:true, key:5},
-        {dataField:'Sucursal', text:'Sucursal',sort:true, key:6},
-        {dataField:'btn',  text:'Modificar', formatter: btnModUSer, key:7},
-        {dataField:'btn2', text:'Eliminar', formatter: btnDeleteUSer, key:8},
-        {dataField:'btn3', text:'Contraseña', formatter: btnModUserPass, key: 9}
+        {dataField: 'Id', text: 'Id', sort: true, key: 1, hidden: true},
+        {dataField: 'Nombre', text: 'Nombre', sort: true, key: 2},
+        {dataField: 'Correo', text: 'Correo', sort: true, key: 3},
+        {dataField: 'Numero', text: 'Telefono', sort: true, key: 4},
+        {dataField: 'Rol', text: 'Rol', sort: true, key: 5},
+        {dataField: 'Sucursal', text: 'Sucursal', sort: true, key: 6},
+        {dataField: 'btn', text: 'Modificar', formatter: btnModUSer, key: 7},
+        {dataField: 'btn2', text: 'Eliminar', formatter: btnDeleteUSer, key: 8},
+        {dataField: 'btn3', text: 'Contraseña', formatter: btnModUserPass, key: 9},
+        {dataField: 'IdRol', text: 'Rol', sort: true, key: 10, hidden: true},
+        {dataField: 'IdSucursal', text: 'Sucursal', sort: true, key: 11, hidden: true},
     ]
 
     const pagination = paginationFactory({
