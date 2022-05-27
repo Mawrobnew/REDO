2476c925afcfe155e7a5f42034aaeaf8002538fe
Tony-Jaime02 <Antonio_Jaime1G@hotmail.com>
2022-05-27 00:21:24 -0500

    Change IP for test in AWS

--- src/component/SidebarData_SU.js
+++ src/component/SidebarData_SU.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as RiIcons from 'react-icons/ri';
-import { faGlobe, faPowerOff} from '@fortawesome/free-solid-svg-icons'
+import { faGlobe} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const SidebarData = [
   {
