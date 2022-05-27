164cec406482c0db470784132712c5db9b35df1e
Tony-Jaime02 <Antonio_Jaime1G@hotmail.com>
2022-05-27 10:05:00 -0500

    Change IP for test in AWS

--- src/utils/WebRequestMiddleware.js
+++ src/utils/WebRequestMiddleware.js
@@ -1,5 +1,5 @@
-//const HOST = "http://44.195.187.31:8080/api"
-const HOST = "http://localhost:8080/api"
+const HOST = "http://44.195.187.31:8080/api"
+//const HOST = "http://localhost:8080/api"
 export const Request = async (method = 'POST', route = '/', data = {}) => {
     //Request configuration
     console.log(route,method)
