36424cde1f7839628a1edd93397c68d06a25b3e1
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-05-30 17:57:28 -0500

    Create Petition functionality added

--- src/component/M_CreatePetition.js
+++ src/component/M_CreatePetition.js
@@ -1,78 +1,29 @@
-import React, {useState} from 'react'
-import {Request} from "../utils/WebRequestMiddleware";
 import '../css/modal.css';
 import {faBoxOpen} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import ValidationModal from "./API/ValidationModal";
+import Select from "./API/Select";
 
-export default function M_CreatePetition(){
-    const [modalInfo, setModalInfo] = useState({
-        name: '',
-        mail: '',
-        password: '',
-        phone: '',
-        rol: '',
-        branch: '',
-    })
-    const [isOpen, setIsOpen] = useState(false)
-    //TODO: FETCH THIS VALUES FROM THE API LATER
-    const rolInventory = [
-        {id:1, label: "Super Usuario"},
-        {id:2, label: "Trabajo Social"},
-        {id:3, label: "Cajero"},
-    ]
-    const branchInventory = [
-        {id:1, label: "Cuernavaca"},
-        {id:2, label: "Jiutepec"},
-        {id:3, label: "Temixco"},
-    ]
-
-    // updates the state on every change of the inputs or the selects
-    const handleInputChange = (event) =>{
-        const {name,value} = event.target
-        setModalInfo({
-            ...modalInfo,
-            [name]:value
-        })
-    }
-
-    //When the form is ready post the modal data to the backend and prevents the default behaviour of the form
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const result = await Request('POST', '/user', modalInfo)
-        const {done, errors} = result
-        if (done) setIsOpen(!isOpen)
-        //TODO: DISPLAY IN RED THE ERRORS OF THE FIELDS
-        console.log("TONY THIS ARE THE ERRORS TO BE DISPLAYED",errors)
-    }
-    //TODO: CREATE FIELD AND SELECT COMPONENTS THAT HANDLE REPEATED LOGIC
-    if (!isOpen) return (
-        <button onClick={() => setIsOpen(true)} id='btnModalCreatePetition'><FontAwesomeIcon icon={faBoxOpen} size='2x'/> Petición de paquetes</button>
-    )
+export default function M_CreatePetition() {
     return (
-        <div>
-            <div className='wrapper' onClick={()=>{setIsOpen(false)}}/>
-            <div className='window'>
-                <button className='closeBtn' onClick={()=>{setIsOpen(false)}}>X</button>
-                <p className='title'>Crear una comunidad</p>
-                <form onSubmit={handleSubmit}>
-                    <div className='formulario'>
-                        <p>Comunidad</p>
-                        <select onChange={handleInputChange} name="rol">
-                            {rolInventory.map((rol)=>(
-                                <option value={rol.id}>{rol.label}</option>
-                            ))}
-                        </select>
-                        <p>Número de paquetes</p>
-                        <input type={"number"} max={999} min={1} required/>
-                        <p>Encargado de comité</p>
-                        <select onChange={handleInputChange} name="rol">
-                            {rolInventory.map((rol)=>(
-                                <option value={rol.id}>{rol.label}</option>
-                            ))}
-                        </select>
-                        <button className='aceptBtn'>Crear petición</button>
-                    </div>
-                </form>
+        <ValidationModal
+            Button={
+                <button id='btnModalCreatePetition'><FontAwesomeIcon icon={faBoxOpen} size='2x'/> Petición de paquetes
+                </button>
+            }
+            route="/request"
+            method="POST"
+        >
+            <p className='title'>Pedir paquete</p>
+            <div className='formulario'>
+                <p>Comunidad</p>
+                <Select name="idCommunity" route="/communities"/>
+                <p>Número de paquetes</p>
+                <input name="quantity" type={"number"} max={999} min={1} required/>
+                <p>Encargado de comité</p>
+                <Select name="idCommitteeMember" route="/committeeMembers"/>
+                <button className='aceptBtn' name="formButton">Crear petición</button>
             </div>
-        </div>)
+        </ValidationModal>
+    )
 }
