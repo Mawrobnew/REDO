278440284a7f9c592ebcebe4bb92548ab3eb2dc2
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-05-31 13:33:22 -0500

    Form upload refactor to handle respose from the file uploaded

--- src/component/M_UploadDocuments.js
+++ src/component/M_UploadDocuments.js
@@ -1,69 +1,74 @@
 import React, {useRef, useState} from 'react'
-import {HOST,FileHost, Request} from "../utils/WebRequestMiddleware";
+import {HOST, FileHost, Request} from "../utils/WebRequestMiddleware";
 import '../css/modal.css';
-import { faFileArchive } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFileArchive} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import M_Fail from "./M_Fail";
+import M_Success from "./M_Success";
 
 
 export default function M_UploadDocuments() {
-    const [modalInfo, setModalInfo] = useState({
-        name: '',
-        mail: '',
-        password: '',
-        phone: '',
-        rol: '',
-        branch: '',
-    })
     const [isOpen, setIsOpen] = useState(false)
-    //TODO: FETCH THIS VALUES FROM THE API LATER
-    const rolInventory = [
-        {id:1, label: "Super Usuario"},
-        {id:2, label: "Trabajo Social"},
-        {id:3, label: "Cajero"},
-    ]
-    const branchInventory = [
-        {id:1, label: "Cuernavaca"},
-        {id:2, label: "Jiutepec"},
-        {id:3, label: "Temixco"},
-    ]
+    const [petitionState, setPetitionState] = useState({
+        successful: false,
+        failed: false
+    })
 
-    // updates the state on every change of the inputs or the selects
-    const handleInputChange = (event) =>{
-        const {name,value} = event.target
-        setModalInfo({
-            ...modalInfo,
-            [name]:value
-        })
-    }
-
-    //When the form is ready post the modal data to the backend and prevents the default behaviour of the form
-    const form = useRef()
     const handleSubmit = async (event) => {
-        const formData= new FormData(form.current)
-        const [result, code] = await Request ('POST', '/', formData)
-        const {done} = result
-        if (done) setIsOpen(!isOpen)
-        event.preventDefault()
+        event.preventDefault();
+        const formData = new FormData();
+        const inputFile = document.getElementById("archivo");
+        for (const file of inputFile.files) {
+            console.log(file)
+            formData.append("archivo", file);
+        }
+        const response = await fetch("http://localhost:9000/", {
+            method: "POST",
+            body: formData,
+            mode: 'cors'
+        })
+        const json = await response.json()
+        console.log(response.status)
+        if (response.status !== 200) {
+            console.log("Errorrrr")
+            setPetitionState({
+                ...petitionState,
+                failed: true
+            });
+        }
+        setIsOpen(false)
+        setPetitionState({
+            ...petitionState,
+            successful: true
+        })
     }
     //TODO: CREATE FIELD AND SELECT COMPONENTS THAT HANDLE REPEATED LOGIC
     if (!isOpen) return (
-        <button onClick={() => setIsOpen(true)} id='btnModalUploadDocs'><FontAwesomeIcon icon={faFileArchive} size='1x'/></button>
+        <div>
+            <M_Success open={petitionState.successful}
+                       onClose={() => setPetitionState({...petitionState, successful: false})}/>
+            <button onClick={() => setIsOpen(true)} id='btnModalUploadDocs'><FontAwesomeIcon icon={faFileArchive}
+                                                                                             size='1x'/></button>
+        </div>
     )
     return (
         <div>
             <div className='wrapper' onClick={()=>{setIsOpen(false)}}/>
             <div className='window'>
-                <button className='closeBtn' onClick={()=>{setIsOpen(false)}}>X</button>
+                <button className='closeBtn' onClick={() => {
+                    setIsOpen(false)
+                }}>X
+                </button>
                 <p className='title'>Subir documentos</p>
-                <form  action={FileHost} target={HOST+'/trabajoSocial'} method='POST' enctype='multipart/form-data'>
+                <form onSubmit={handleSubmit} action={FileHost} target={HOST + '/trabajoSocial'} method='POST'
+                      enctype='multipart/form-data'>
                     <div className='formulario'>
-
                         <p>Credencial</p>
-                        <input required type="file" id="archivo" name="archivo" accept=".jpg, .jpeg, .png, .pdf"/>
-
+                        <input type="file" required id="archivo" name="archivo" accept=".jpg, .jpeg, .png, .pdf"/>
                         <button className='aceptBtn'>Enviar</button>
                     </div>
                 </form>
             </div>
+            <M_Fail open={petitionState.failed} onClose={() => setPetitionState({...petitionState, failed: false})}/>
         </div>)
 }
